cdb997392f4f2a4454edba18bc4c1b30aabcfc4f
Tracey Saada <tracey.saada@outlook.com>
2021-02-01 19:28:10 +0000

    change some settings in burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,18 @@
 const orm = require("../config/orm.js");
 
-const burger = {
+const burgers = {
   all: function (cb) {
-    orm.all("burgers", function (res) {
+    orm.selectAll("burgers", function (res) {
       cb(res);
     });
   },
   create: function (cols, vals, cb) {
-    orm.create("burgers", cols, vals, function (res) {
+    orm.insertOne("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
   update: function (objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function (res) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
       cb(res);
     });
   },
