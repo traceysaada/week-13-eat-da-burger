42525aaca60266a52148207a40873eda4a710344
Tracey Saada <tracey.saada@outlook.com>
2021-02-01 19:48:43 +0000

    set up server

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const app = express();
+const routes = require("./controllers/burgers_controller");
+const expressHandleBars = require("express-handlebars");
 const PORT = process.env.PORT || 3000;
-const orm = require("./config/orm.js");
-
-const data = orm.selectWhere();
-
-console.log(data); 
-
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-
+app.use(routes);
+app.engine("handlebars", expressHandleBars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 
 
