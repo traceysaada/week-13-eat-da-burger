b65708c02273d192104acb4936c8808ea2f4f774
Tracey Saada <tracey.saada@outlook.com>
2021-02-01 18:37:35 +0000

    set up orm.js and models folder inc burger.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const orm = require("./config/orm.js");
+
+const data = orm.selectWhere();
+
+console.log(data); 
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
